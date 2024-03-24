9f98b837672b9b040d7f7589a9c10e0cff7a0093
Daniel S Goncalves <dsg1407@gmail.com>
2024-03-23 23:29:51 -0300

    define application theme and component loading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '@/theme'
+
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
+
+import { Home } from '@/screens/Home'
+import { Loading } from '@/components/Loading'
+//TODO ROUTES
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <Loading />}
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+    </ThemeProvider>
+  )
+}
